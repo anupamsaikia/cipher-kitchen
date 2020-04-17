1e5657f5a0754830bb8f4be40b3712c2bcfc1977
Anupam Saikia <ianupamsaikia@gmail.com>
2020-04-17 21:02:52 +0530

    improve, refactor, added decrypt()

--- src/ciphers/caesar.js
+++ src/ciphers/caesar.js
@@ -3,31 +3,55 @@
  * @param {number} shift Shift value
  */
 export function encrypt(message, shift) {
+  // check for correct type
   if (typeof shift != "number") {
-    console.error("shift should be a number");
+    console.error("expected type: number");
     return;
   }
-
   if (typeof message != "string") {
-    console.error("message should be a string");
+    console.error("expected type: string");
     return;
   }
 
-  if (shift < 0) {
-    return encrypt(message, shift + 26);
-  } else {
-    return message.replace(/[A-Za-z]/g, match => {
-      // ascii code of the matched character
-      let code = match.charCodeAt(0);
+  while (shift < 0) shift += 26;
+  return caesarShift(message, shift);
+}
 
-      // Uppercase letters
-      if (code >= 65 && code <= 90) {
-        return String.fromCharCode(((code - 65 + shift) % 26) + 65);
-      }
-      // Lowercase letters
-      else {
-        return String.fromCharCode(((code - 97 + shift) % 26) + 97);
-      }
-    });
+/**
+ * @param {string} message The ciphertext message
+ * @param {number} shift Shift value
+ */
+export function decrypt(message, shift) {
+  // check for correct type
+  if (typeof shift != "number") {
+    console.error("expected type: number");
+    return;
   }
+  if (typeof message != "string") {
+    console.error("expected type: string");
+    return;
+  }
+
+  while (shift < 0) shift += 26;
+
+  shift = shift % 26;
+  shift = 26 - shift;
+  return caesarShift(message, shift);
+}
+
+/**
+ *
+ * @param {string} message
+ * @param {number} shift
+ */
+function caesarShift(message, shift) {
+  return message.replace(/[A-Za-z]/g, match => {
+    // ascii code of the matched character
+    let code = match.charCodeAt(0);
+
+    // handle uppercase & lowercase letters
+    if (code >= 65 && code <= 90)
+      return String.fromCharCode(((code - 65 + shift) % 26) + 65);
+    else return String.fromCharCode(((code - 97 + shift) % 26) + 97);
+  });
 }
