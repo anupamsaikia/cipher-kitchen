72a1fb5bf3864c0827f0434f5b3b23856270ea07
Anupam Saikia <ianupamsaikia@gmail.com>
2020-04-17 16:43:23 +0530

    add component selector

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,46 @@
 <template>
-  <h1>hello</h1>
+  <div>
+    <v-row justify="center">
+      <v-col xs="12" sm="8" md="6">
+        <v-select
+          :items="algorithms"
+          v-model="selectedAlgo"
+          label="Select cipher algorithm"
+          outlined
+          filled
+          dense
+          color="teal"
+        ></v-select>
+      </v-col>
+    </v-row>
+
+    <component :is="selectedAlgo"></component>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import Caesar from "../components/Caesar.vue";
 export default {
-  name: "Home"
+  name: "Home",
+  data: () => ({
+    selectedAlgo: "caesar",
+    algorithms: [
+      {
+        text: "Caesar cipher",
+        value: "caesar"
+      },
+      {
+        text: "Playfair cipher",
+        value: "playfair"
+      },
+      {
+        text: "DES cipher",
+        value: "des"
+      }
+    ]
+  }),
+  components: {
+    Caesar
+  }
 };
 </script>
