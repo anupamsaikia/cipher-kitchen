2f9b71f8066b73fa8db07ffd185dda85875d7531
Anupam Saikia <ianupamsaikia@gmail.com>
2020-04-19 14:09:16 +0530

    remove encrypt btn, make cipherText computed prop

--- src/components/CaesarEncrypt.vue
+++ src/components/CaesarEncrypt.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-row>
-      <v-col xs="6" class="pb-0">
+    <v-row justify="center">
+      <v-col sm="6" class="pb-0">
         <v-textarea
           outlined
           name="plaintext"
@@ -9,7 +9,7 @@
           v-model="plainText"
         ></v-textarea>
       </v-col>
-      <v-col xs="6" class="pb-0">
+      <v-col sm="6" class="pb-0">
         <v-textarea
           outlined
           filled
@@ -22,40 +22,42 @@
       </v-col>
     </v-row>
 
-    <v-row justify="center">
-      <v-col xs="12" sm="8" md="6" class="pb-0 pt-0">
-        <v-text-field
-          label="Enter shift value here"
-          prepend-inner-icon="mdi-key-variant"
-          type="number"
-          outlined
-          dense
-          v-model="shift"
-        ></v-text-field>
-      </v-col>
-    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-row justify="center">
+          <v-col cols="12" class="pb-0 pt-0">
+            <v-text-field
+              v-model="shift"
+              type="number"
+              label="Enter shift value here"
+              outlined
+              prepend-inner-icon="mdi-key-variant"
+              append-outer-icon="mdi-plus"
+              @click:append-outer="increment"
+              prepend-icon="mdi-minus"
+              @click:prepend="decrement"
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row justify="center">
-      <v-col xs="12" sm="8" md="6">
-        <v-btn block color="orange" dark @click="encryptMsg">
-          <v-icon left dark>mdi-lock</v-icon>
-          Encrypt
-        </v-btn>
+        <v-row justify="center">
+          <v-col cols="6" sm="12" md="6">
+            <v-btn block color="grey darken-1" dark>
+              <v-icon left dark>mdi-content-copy</v-icon>
+              Copy plaintext
+            </v-btn>
+          </v-col>
+          <v-col cols="6" sm="12" md="6">
+            <v-btn block color="grey darken-1" dark>
+              <v-icon left dark>mdi-content-copy</v-icon>
+              Copy ciphertext
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-col>
-    </v-row>
 
-    <v-row justify="center">
-      <v-col xs="6" sm="4" md="3">
-        <v-btn block color="grey darken-1" dark>
-          <v-icon left dark>mdi-content-copy</v-icon>
-          Copy plaintext
-        </v-btn>
-      </v-col>
-      <v-col xs="6" sm="4" md="3">
-        <v-btn block color="grey darken-1" dark>
-          <v-icon left dark>mdi-content-copy</v-icon>
-          Copy ciphertext
-        </v-btn>
+      <v-col cols="12" sm="6">
+        <h3>Character mapping</h3>
       </v-col>
     </v-row>
   </div>
@@ -66,19 +68,26 @@ import { encrypt } from "../ciphers/caesar";
 export default {
   data: () => ({
     plainText: "",
-    cipherText: "",
-    shift: null
+    shift: 0
   }),
-  methods: {
-    encryptMsg: function() {
-      const shift = parseInt(this.shift);
+  computed: {
+    cipherText: function() {
+      let shift = parseInt(this.shift, 10);
 
       if (isNaN(shift)) {
         // use 0 as default value
-        this.cipherText = encrypt(this.plainText, 0);
+        return encrypt(this.plainText, 0);
       } else {
-        this.cipherText = encrypt(this.plainText, shift);
+        return encrypt(this.plainText, shift);
       }
+    }
+  },
+  methods: {
+    increment() {
+      this.shift = parseInt(this.shift, 10) + 1;
+    },
+    decrement() {
+      this.shift = parseInt(this.shift, 10) - 1;
     }
   }
 };
