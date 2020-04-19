94e9bd2ccf451096c1b8ce2111991ad5348ba12c
Anupam Saikia <ianupamsaikia@gmail.com>
2020-04-19 15:13:59 +0530

    remove decrypt & copy btn, make plainText computed

--- src/components/CaesarDecrypt.vue
+++ src/components/CaesarDecrypt.vue
@@ -23,39 +23,18 @@
     </v-row>
 
     <v-row justify="center">
-      <v-col xs="12" sm="8" md="6" class="pb-0 pt-0">
+      <v-col cols="12" sm="6" class="pb-0 pt-0">
         <v-text-field
-          label="Enter shift value here"
-          prepend-inner-icon="mdi-key-variant"
-          type="number"
-          outlined
-          dense
           v-model="shift"
+          type="number"
+          label="Enter shift value here"
+          outlined
+          prepend-inner-icon="mdi-key-variant"
+          append-outer-icon="mdi-plus"
+          @click:append-outer="increment"
+          prepend-icon="mdi-minus"
+          @click:prepend="decrement"
         ></v-text-field>
-      </v-col>
-    </v-row>
-
-    <v-row justify="center">
-      <v-col xs="12" sm="8" md="6">
-        <v-btn block color="green darken-1" dark @click="encryptMsg">
-          <v-icon left dark>mdi-lock-open</v-icon>
-          Decrypt
-        </v-btn>
-      </v-col>
-    </v-row>
-
-    <v-row justify="center">
-      <v-col xs="6" sm="4" md="3">
-        <v-btn block color="grey darken-1" dark>
-          <v-icon left dark>mdi-content-copy</v-icon>
-          Copy ciphertext
-        </v-btn>
-      </v-col>
-      <v-col xs="6" sm="4" md="3">
-        <v-btn block color="grey darken-1" dark>
-          <v-icon left dark>mdi-content-copy</v-icon>
-          Copy plaintext
-        </v-btn>
       </v-col>
     </v-row>
   </div>
@@ -66,19 +45,27 @@ import { decrypt } from "../ciphers/caesar";
 export default {
   data: () => ({
     cipherText: "",
-    plainText: "",
-    shift: null
+    shift: 0
   }),
-  methods: {
-    encryptMsg: function() {
-      const shift = parseInt(this.shift);
+  computed: {
+    plainText: function() {
+      let shift = parseInt(this.shift, 10);
 
       if (isNaN(shift)) {
         // use 0 as default value
-        this.plainText = decrypt(this.cipherText, 0);
+        return decrypt(this.cipherText, 0);
       } else {
-        this.plainText = decrypt(this.cipherText, shift);
+        return decrypt(this.cipherText, shift);
       }
+    }
+  },
+
+  methods: {
+    increment() {
+      this.shift = parseInt(this.shift, 10) + 1;
+    },
+    decrement() {
+      this.shift = parseInt(this.shift, 10) - 1;
     }
   }
 };
