17b69b8caa8e5d463be9c3296dd60aec9cb51091
Anupam Saikia <ianupamsaikia@gmail.com>
2020-04-22 19:50:36 +0530

    add decrypt function

--- src/ciphers/playfair.js
+++ src/ciphers/playfair.js
@@ -60,6 +60,65 @@ export function encrypt(message, keyPhrase) {
 }
 
 /**
+ * Decrypt the given message using playfair cipher
+ * @param {string} message Message to decrypt
+ * @param {string} keyPhrase Keyphrase for Playfair cipher
+ */
+export function decrypt(message, keyPhrase) {
+  message = Array.from(sanitize(message));
+
+  const keyTable = generateKeyTable(keyPhrase);
+  const charPos = getCharPositions(keyTable);
+
+  let plainText = [];
+
+  // If message has odd number of letters then remove the last letter
+  if (message.length % 2 != 0) {
+    message.pop();
+  }
+
+  for (let i = 0; i < message.length; i += 2) {
+    // if the letters are in same row, then use the letters to their immediate left respectively
+    if (charPos[message[i]].row == charPos[message[i + 1]].row) {
+      const x1 = charPos[message[i]].row;
+      const y1 = charPos[message[i]].col ? charPos[message[i]].col - 1 : 4;
+      const x2 = x1;
+      const y2 = charPos[message[i + 1]].col
+        ? charPos[message[i + 1]].col - 1
+        : 4;
+      plainText[i] = keyTable[x1][y1];
+      plainText[i + 1] = keyTable[x2][y2];
+    }
+
+    // if the letters are in same column, then use the letters immediately above respectively
+    else if (charPos[message[i]].col == charPos[message[i + 1]].col) {
+      const x1 = charPos[message[i]].row ? charPos[message[i]].row - 1 : 4;
+      const y1 = charPos[message[i]].col;
+      const x2 = charPos[message[i + 1]].row
+        ? charPos[message[i + 1]].row - 1
+        : 4;
+      const y2 = y1;
+      plainText[i] = keyTable[x1][y1];
+      plainText[i + 1] = keyTable[x2][y2];
+    }
+
+    // if the letters are not on the same row or column,
+    // replace them with the letters on the same row respectively
+    // but at the other pair of corners of the rectangle defined by the original pair.
+    else {
+      const x1 = charPos[message[i]].row;
+      const y1 = charPos[message[i + 1]].col;
+      const x2 = charPos[message[i + 1]].row;
+      const y2 = charPos[message[i]].col;
+
+      plainText[i] = keyTable[x1][y1];
+      plainText[i + 1] = keyTable[x2][y2];
+    }
+  }
+  return plainText.join("");
+}
+
+/**
  * Generates the key table for Playfair cipher
  * @param {string} keyPhrase Key phrase for Playfair cipher
  * @returns {Array} Array[5][5] representing letters in key table
