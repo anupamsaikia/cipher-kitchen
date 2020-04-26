1934e96f5cfb850535864fd87235704178eb0087
Anupam Saikia <ianupamsaikia@gmail.com>
2020-04-26 21:17:16 +0530

    add notes to Playfair component

--- src/components/Playfair.vue
+++ src/components/Playfair.vue
@@ -19,6 +19,19 @@
         <playfair-decrypt></playfair-decrypt>
       </v-tab-item>
     </v-tabs-items>
+
+    <v-divider class="mt-5 mb-5"></v-divider>
+
+    <div>
+      <p class="subtitle-2 mb-0">Note:</p>
+      <p class="caption mb-0">
+        Here the Playfair algorithm combines the letter "I" and "J" which is
+        represented as "I".
+      </p>
+      <p class="caption">
+        The letter "X" is used as filler character
+      </p>
+    </div>
   </div>
 </template>
 
