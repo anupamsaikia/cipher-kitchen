79289f702c4abc4cc4682d08e0265631cee8ab33
Anupam Saikia <ianupamsaikia@gmail.com>
2020-04-28 00:30:56 +0530

    add about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1 class="mt-5 pt-5 pb-5" style="text-align:center">
+      <span class="red--text display-3 font-weight-thin">Cipher</span>
+      <span class="display-3 font-weight-thin"> Kitchen</span>
+      <span class="overline"> V1.0.0</span>
+    </h1>
+    <h2 class="headline font-weight-light" style="text-align:center">
+      A tool to experiment with different cipher algorithms
+    </h2>
+
+    <v-divider class="mt-5 mb-5"></v-divider>
+
+    <p>
+      If you have any issues, suggestions or want to contribute to this project,
+      please visit the
+      <a href="https://github.com/anupamsaikia/cipher-kitchen" target="_blank"
+        >Github repository</a
+      >.
+    </p>
+
+    <v-divider class="mt-5 mb-5"></v-divider>
+
+    <p class="font-weight-bold">Big Thanks:</p>
+    <p>
+      - To the
+      <a href="https://github.com/vuejs/vue" target="_blank">Vue.js</a> team for
+      their awesome JavaScript framework.
+    </p>
+
+    <p>
+      - To the <a href="https://vuetifyjs.com" target="_blank">Vuetify</a> team
+      for their beautiful material design component framework.
+    </p>
+    <p>
+      - To
+      <a href="https://github.com/brix/crypto-js" target="_blank">CryptoJS</a>
+      for a great JavaScript library of crypto standards.
+    </p>
+
+    <v-divider class="mt-5 mb-5"></v-divider>
+    <div class="caption" style="text-align:center">
+      <a href="https://spdx.org/licenses/MIT.html" target="_blank">MIT</a>
+      License Copyright (c) 2020 Anupam Saikia
+    </div>
   </div>
 </template>
